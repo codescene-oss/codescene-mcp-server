9c416574c875590c22b8ff471d74153a5309d67f
Adam Tornhill <adam@adamtornhill.com>
2025-11-19 10:44:21 +0100

    Implement the refactoring scenarios based on CodeScene's benchmarking data

--- src/code_health_tools/business_case.py
+++ src/code_health_tools/business_case.py
@@ -1,8 +1,50 @@
 from code_health_tools.s_curve import metrics
 
+# This module estimates the outcome of improving Code Health in terms of 
+# faster development time and fewer defects.
+#
+# The theoretical background and research is 
+# summarized here: https://codescene.com/blog/benchmarking-code-health-refactoring-roi
+#
+# We model three different scenarios:
+TARGET_SCENARIOS = [
+    {'scenario': 'Improve Code Health to the industry average.',
+     'target_code_health': 5.15},
+     {'scenario': 'Improve Code Health to the level of top 5% performers.',
+      'target_code_health': 9.1},
+    {'scenario': 'Maximize your speed and quality with a perfect Code Health score.',
+     'target_code_health': 10.0}
+]
+
 def _outcome_of_refactoring_from(current_code_health):
     """Estimates the refactoring outcome in terms of development speed and defect reduction."""
-    return 'improved'
+    for scenario in TARGET_SCENARIOS:
+        scenario_models_improvement = current_code_health < scenario['target_code_health']
+
+        if scenario_models_improvement:
+            return _build_business_case_for(scenario, current_code_health)
+        
+    return f'Your Code Health of {current_code_health} is already perfect. Keep up the good!'
+
+def _build_business_case_for(scenario, current_code_health):
+    estimated_outcomes = metrics.collect(code_health_baseline=current_code_health, code_health_target=scenario['target_code_health'])
+
+    (defect_optimistic, defect_pessimistic) = estimated_outcomes['defects']
+    (time_optimistic, time_pessimistic) = estimated_outcomes['time']
+
+    estimate = {'title': 'Business case to motivate refactoring for improved Code Health',
+                'data_description': '''
+                The business case models two scenario: optimistic and pessimistic.
+                CodeScene's model predicts that there's a 90 percent chance that the
+                actual measured outcome falls in this range.
+                ''',
+                'current_code_health': current_code_health,
+                'optimistic_outcome': {'defect_reduction':              abs(defect_optimistic),
+                                       'development_speed_improvement': abs(time_optimistic)},
+                'pessimistic_outcome': {'defect_reduction':              abs(defect_pessimistic),
+                                       'development_speed_improvement':  abs(time_pessimistic)}}
+
+    return estimate | scenario
 
 ## Basic input validation
 
@@ -26,6 +68,4 @@ def make_business_case_for(current_code_health):
     """
     _validate_code_health_score("current_code_health", current_code_health)
 
-    return {
-        "outcome": _outcome_of_refactoring_from(current_code_health),
-    }
+    return {'outcome': _outcome_of_refactoring_from(current_code_health)}
--- src/code_health_tools/test_business_case.py
+++ src/code_health_tools/test_business_case.py
@@ -15,6 +15,66 @@ class TestMakeBusinessCaseFor(unittest.TestCase):
             res = make_business_case_for(current)
             self.assertIn("outcome", res, name)
 
+    def test_outcome_for_unhealthy_code(self):
+        the_case = self._outcome_for(current_code_health=3.9)
+
+        self.assertEqual({
+             'title': 'Business case to motivate refactoring for improved Code Health', 
+             'current_code_health': 3.9, 
+             'optimistic_outcome': {
+                  'defect_reduction': 27.33, 
+                  'development_speed_improvement': 11.89}, 
+             'pessimistic_outcome': {
+                  'defect_reduction': 10.66, 
+                  'development_speed_improvement': 1.91}, 
+             'scenario': 'Improve Code Health to the industry average.', 'target_code_health': 5.15},
+             the_case)
+    
+    def test_problematic_code_above_industry_baseline(self):
+        the_case = self._outcome_for(current_code_health=5.2)
+        
+        self.assertEqual({
+            'title': 'Business case to motivate refactoring for improved Code Health', 
+            'current_code_health': 5.2, 
+            'optimistic_outcome': {
+                'defect_reduction': 49.41, 
+                'development_speed_improvement': 42.78}, 
+            'pessimistic_outcome': {
+                'defect_reduction': 31.63, 
+                'development_speed_improvement': 26.81}, 
+            'scenario': 'Improve Code Health to the level of top 5% performers.', 
+            'target_code_health': 9.1},
+             the_case)
+    
+    def test_healthy_top_performers_code(self):
+        the_case = self._outcome_for(current_code_health=9.3)
+        
+        self.assertEqual({
+            'title': 'Business case to motivate refactoring for improved Code Health', 
+            'current_code_health': 9.3, 
+            'optimistic_outcome': {
+                'defect_reduction': 58.91, 
+                'development_speed_improvement': 26.9}, 
+            'pessimistic_outcome': {
+                'defect_reduction': 47.94, 
+                'development_speed_improvement': 12.73}, 
+            'scenario': 'Maximize your speed and quality with a perfect Code Health score.', 
+            'target_code_health': 10.0},
+             the_case)
+    
+    def test_perfect_ten_code(self):
+        the_case = make_business_case_for(current_code_health=10.0)
+        outcome = the_case['outcome']
+
+        self.assertEqual('Your Code Health of 10.0 is already perfect. Keep up the good!', outcome)
+
+    def _outcome_for(self, current_code_health):
+        the_case = make_business_case_for(current_code_health)
+        outcome = the_case['outcome']
+        outcome.pop('data_description') # data_description is a verbose static string intended for the LLMs
+        return outcome
+
+
     ERRONEOUS_CASES = [
 		("below_min",    0.999),
 		("above_max",    10.0001),
