e688f25d15ca280bbcd5122a5b0677d068f1ae95
Asko Nõmm <asko.nomm@codescene.com>
2025-12-10 16:01:33 +0200

    CS-5882: Implement `track` method decorator for analytics

---
+++ src/utils/track.py
@@ -0,0 +1,23 @@
+import functools
+import os
+import requests
+from utils import get_api_url, get_api_request_headers
+
+
+def track(event_type: str):
+	def wrapper(f):
+		@functools.wraps(f)
+		def wrapped(self, *f_args, **f_kwargs):
+			if os.getenv("CS_DISABLE_ANALYTICS") is None:
+				payload = {
+					"event-type": f"mcp-{event_type}", 
+					"event-properties": {}
+				}
+
+				requests.post(f"{get_api_url()}/v2/analytics/track", headers=get_api_request_headers(), json=payload)
+
+			return f(self, *f_args, **f_kwargs)
+
+		return wrapped
+
+	return wrapper
