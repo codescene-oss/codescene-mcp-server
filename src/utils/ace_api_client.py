2ede7a8759e28c0eb9b104fe5afefef46de9c60f
Kalle Norrestam <kalle@norrestam.se>
2025-12-11 11:50:28 +0100

    CS-5894: Add tests for ace api module

--- src/utils/ace_api_client.py
+++ src/utils/ace_api_client.py
@@ -4,8 +4,8 @@ import requests
 class HttpClientError(Exception):
     pass
 
-def get_api_url() -> str:
-    return "https://devtools.codescene.io"
+def get_api_endpoint(endpoint: str) -> str:
+    return f"https://devtools.codescene.io/{endpoint}"
 
 def get_api_request_headers() -> dict:
     return {
@@ -13,29 +13,34 @@ def get_api_request_headers() -> dict:
     }
 
 def post(endpoint: str, json_payload: dict) -> dict:
-    url = f"{get_api_url()}/{endpoint}"
-    return requests.post(url, json=json_payload, headers=get_api_request_headers())
+    return requests.post(get_api_endpoint(endpoint), json=json_payload, headers=get_api_request_headers())
 
 def _is_client_error(response: requests.Response) -> bool:
-   return response.status_code >= 400 and response.status_code < 500 
+    return response.status_code >= 400 and response.status_code < 500 
 
 def _is_json(response: requests.Response) -> bool:
-   return response.headers.get('content-type').find('application/json') >= 0
+    return response.headers.get('content-type').find('application/json') >= 0
 
-def retrying_post(n: int, endpoint: str, json_payload: dict) -> dict:
-  r = post(endpoint, json_payload)
-  if r.ok:
-    return r.json()
-  if 1 < n and r.status_code in [408, 504]:
-    print("Retry post...")
-    return retrying_post(n - 1, endpoint, json_payload)
+def retrying_post(n: int, endpoint: str, json_payload: dict) -> requests.Response:
+    r = post(endpoint, json_payload)
+    if 1 < n and r.status_code in [408, 504]:
+        print("Retry post...")
+        return retrying_post(n - 1, endpoint, json_payload)
+    return r
   
-  # devtools-portal and ACE provides error details as json on bad requests
-  if _is_client_error(r) and _is_json(r):
-    error = r.json()['error']
-    raise HttpClientError(f"HttpClientError {r.status_code}: {error}")
+def validate_response(response: requests.Response):
+    if response.ok:
+        return
   
-  r.raise_for_status()
+    # devtools-portal and ACE provides error details as json on bad requests
+    if _is_client_error(response) and _is_json(response):
+        error = response.json().get('error', "<unknown-error>")
+        raise HttpClientError(f"HttpClientError {response.status_code}: {error}")
+  
+    response.raise_for_status()
 
 def post_refactor(json_payload: dict) -> dict:
-    return retrying_post(3, 'api/refactor', json_payload)
+    r = retrying_post(3, 'api/refactor', json_payload)
+    validate_response(r)
+    return r.json()
+
